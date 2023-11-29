fb7cce3055ea998960912cc89fb224cb68e802e8
ABDALLAH Karim <karim.abdallah@etu.univ-amu.fr>
2023-11-29 15:23:46 +0100

    JOB DONE

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import os , random
 
-for i in range(100):
+n = random.randrange(1, 120)
+for i in range(n):
     d = str(i) + 'days ago'
     rand = random.randrange(1, 12)
     with open('test.txt','a') as file:
         file.write(d+'\n')
     os.system('git add test.txt')
-    os.system('git commit --date=" 2023-'+str(rand)+'-'+d+'" -m 1')
+    os.system('git commit --date=" 2021-'+str(rand)+'-'+d+'" -m 1')
 os.system('git push -u origin main')
